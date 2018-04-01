0fe982d77f023f69594a4d98a6d79d17dc203bd1
Yuri <creator.and.dev@gmail.com>
2018-04-01 23:48:20 +0300

    + add onClick Find beer + edit activity java

--- app/src/main/java/hfad/com/beeradviser/FindBearActivity.java
+++ app/src/main/java/hfad/com/beeradviser/FindBearActivity.java
@@ -9,5 +9,6 @@ public class FindBearActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_bear);
+
     }
 }
