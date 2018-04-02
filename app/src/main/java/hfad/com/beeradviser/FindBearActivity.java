2ba8ed6a87ef13a77b736d22d9a32e5f9e60453d
Yuri <creator.and.dev@gmail.com>
2018-04-02 16:02:26 +0300

    create in activity choice link a r.id

--- app/src/main/java/hfad/com/beeradviser/FindBearActivity.java
+++ app/src/main/java/hfad/com/beeradviser/FindBearActivity.java
@@ -3,6 +3,8 @@ package hfad.com.beeradviser;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBearActivity extends Activity {
 
@@ -11,9 +13,22 @@ public class FindBearActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_bear);
 
-        //call when the user clicks the button
-        public void onClickFindBeer(View view) {
 
-        }
+    }
+
+    //call when the user clicks the button
+    public void onClickFindBeer(View view) {
+
+        //get link on TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //get link on Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //get variant, choice in Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //output choice variant
+        brands.setText(beerType);
     }
 }
